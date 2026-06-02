059b0ccf0eabdda3f4ecb5b7f9dc0d54cfefd80a
cm5wxjmcpv-bit <cm5wxjmcpv@privaterelay.appleid.com>
2026-06-02 08:29:54 -0400

    Update APPS_SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
   APP_NAME: "MAC Industries Invoice App",
   COMPANY_NAME: "MAC Industries",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxRNm5lbJ39sJWIspSKVfGj8ePkTD5MoXT-ThtLM7n6BHwvCcymPJlPnxNJIxbbAKJT/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw1Ofl011icY9zWO3BdSRAugeLnZWR4AHem3h959T6ncnjwvCcPMYzMzMdnTmDW-yxq/exec",
   USE_LOCAL_FALLBACK: true
 };
